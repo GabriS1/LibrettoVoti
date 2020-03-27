c1a38f078c45dc0a9b6c9264982ff8eb4593407a
gabri <gabri@DESKTOP-CSDLKIC.homenet.telecomitalia.it>
2020-03-27 13:37:41 +0100

    h

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,25 +1,56 @@
 package it.polito.tdp.libretto.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Memorizza e gestisce un insieme di voti superati.
  * 
- * @author Fulvio
+ * @author gabri
  *
  */
 public class Libretto {
 
-	private List<Voto> voti = new ArrayList<>();
-
+	private List<Voto> voti = new ArrayList<>(); // voti è una variabile che punta ad un oggetto di tipo list; quando
+													// faccio NEW inizializzo il mio puntatore voti, facendolo puntare
+													// ad un arrayList vuota
+	
+	
+	public Libretto() { //costruttore vero e proprio, da mettere se metto anche il costruttore dopo
+		super();
+	}
+	
+	
 	/**
-	 * Aggiunge un nuovo voto al libretto
+	 * Copy Constructor
+	 * "Shallow"-->fa una copia dell'oggetto corrente(lib), ma non fa delle copie degli oggetti contenuti in esso
+	 * Gli oggetti Voto vengono condivisi tra l'array precedente e questa nuovo--> fanno riferimento agli stessi oggetti fisici
+	 * Non duplico gli oggetti Voto
+	 * @param lib
+	 */
+	public Libretto(Libretto lib) { //costruttore 
+		super();
+		this.voti.addAll(lib.voti);
+		
+	}
+	
+	
+	/**
+	 * Aggiunge un nuovo voto al libre tto
 	 * 
 	 * @param v Voto da aggiungere
+	 * @return {@code true} se il voto non è in conflitto e non è duplicato, {@code false} altrimenti
 	 */
-	public void add(Voto v) {
-		this.voti.add(v);
+	public boolean add(Voto v) {
+		
+		if(this.isConflitto(v) || this.isDuplicato(v)) {
+			return false;  //segnalo chiamante che non ha avuto successo
+		}else {
+			this.voti.add(v);
+			return true;
+		}
+		
 	}
 
 	/**
@@ -38,21 +69,22 @@ public class Libretto {
 		}
 		return s;
 	}
-	
+
 	/**
-	 * Genera un nuovo libretto, a partire da quello esistente,
-	 * che conterrà esclusivamenti i voti con votazione pari a quella specificata.
+	 * Genera un nuovo libretto, a partire da quello esistente, che conterrà
+	 * esclusivamenti i voti con votazione pari a quella specificata.
+	 * 
 	 * @param voto votazione specificata
 	 * @return nuovo Libretto "ridotto"
 	 */
 	public Libretto estraiVotiUguali(int voto) {
-		Libretto nuovo = new Libretto() ;
-		for(Voto v: this.voti) {
-			if(v.getVoto()==voto) {
+		Libretto nuovo = new Libretto();
+		for (Voto v : this.voti) {
+			if (v.getVoto() == voto) {
 				nuovo.add(v);
 			}
 		}
-		return nuovo ;
+		return nuovo;
 	}
 
 	public String toString() {
@@ -61,6 +93,114 @@ public class Libretto {
 			s += v.toString() + "\n";
 		}
 		return s;
+	}
+
+	/**
+	 * Dato il nome del corso ricera se il nome del corso esiste nel libretto, in
+	 * caso affermativo ritorna il {@link Voto} corrispondente, in caso negativo
+	 * ritorna {@code null}
+	 * 
+	 * @param nomeCorso nome esame da cercare
+	 * @return il {@link Voto} corrispondente, oppure {@code null} se non esiste
+	 */
+	public Voto cercaNomeCorso(String nomeCorso) {
+		/*
+		 * for(Voto v: this.voti) { if(nomeCorso.equals(v.getCorso())) { return v; } }
+		 * return null;
+		 */
+
+		int pos = this.voti.indexOf(new Voto(nomeCorso, 0, null));
+		if (pos == -1) {
+
+			return null;
+
+		} else {
+			return voti.get(pos);
+		}
+	}
+
+	/**
+	 * Ritorna {@code true} se il corso specificato da {@code v} esiste nel
+	 * libretto, con stessa valutazione. Se non esiste o se la valutazione è diversa
+	 * ritorna {@code false}
+	 * 
+	 * @param v il {@link Voto} da ricercare
+	 * @return l'esistenza del duplicato
+	 */
+	public boolean isDuplicato(Voto v) {
+		Voto esiste = this.cercaNomeCorso(v.getCorso());
+		if (esiste == null) { // non l'ho trovato--> non è duplicato
+			return false;
+		}
+		/*if (esiste.getVoto() == v.getVoto()) {
+			return true;
+		} else {
+			return false;
+		}*/
+		return (esiste.getVoto() == v.getVoto());
+
+	}
+
+	/**
+	 * Detremina se esiste un voto con lo stesso nome corso ma valutazione diverse
+	 * 
+	 * @param v
+	 * @return
+	 */
+	public boolean isConflitto(Voto v) {
+		Voto esiste = this.cercaNomeCorso(v.getCorso());
+		if (esiste == null) { // non l'ho trovato--> non è duplicato
+			return false;
+		}
+		return (esiste.getVoto() != v.getVoto());
+	}
+	
+	public Libretto creaLibrettoMigliorato() {
+		Libretto nuovo = new Libretto();
+		for(Voto v: this.voti) {
+			Voto v2 =  v.clone(); 
+			//Voto v2 =new Voto(v);
+			if(v2.getVoto()>=24) {
+				v2.setVoto(v2.getVoto()+2);
+				if(v2.getVoto()>30) {
+					v2.setVoto(30);
+				}
+			}else {
+					if(v2.getVoto()>=18) {
+						v2.setVoto(v2.getVoto()+1);
+					}
+				}
+			nuovo.add(v2);
+			
+		}
+		
+		return nuovo;
+	}
+	
+	
+	/**
+	 * ordina voti presneti nel libretto corrente alfebiticamente per corso
+	 */
+	public void ordinaPerCorso() {
+		Collections.sort(this.voti);  //ordina la lista in base all'ordine NATURALE degli elementi, implementata in compareTo in Voto
+	}
+	
+	
+	public void ordinaPerVoto() {
+		Collections.sort(this.voti, new ConfrontaVotiPerValutazione());
+	}
+	
+	public void cancellaVotiScarsi() {
+		List<Voto> daRimuovere= new ArrayList<>();
+		for(Voto v: this.voti) {  
+			if(v.getVoto()<24) {
+				daRimuovere.add(v);
+			}
+		}
+		
+		for(Voto v: daRimuovere) {
+			this.voti.remove(v);
+		}
 	}
 
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -4,23 +4,62 @@ import java.time.LocalDate;
 
 public class TestLibretto {
 	
-	Libretto lib ;
+	Libretto lib ; //lo metto qui cosi il libretto è accessibile anche ad altri metodi di questa classe di test
 	
 	private void run() {
 		this.lib = new Libretto() ; // crea libretto vuoto
 		
+		
+		//1_Inserire alcuni voti
 		Voto v1 = new Voto("Tecniche di programmazione", 30, LocalDate.of(2020, 06, 15)) ;
 		Voto v2 = new Voto("Analisi II", 28, LocalDate.of(2020, 06, 28)) ;
 
 		lib.add(v1);
 		lib.add(v2);
 		lib.add(new Voto("Economia", 24, LocalDate.of(2020, 02, 14)));
-		
 		System.out.println(this.lib) ;
 		
+		//2_ stampa tutti i voti = a 28
 		System.out.println(this.lib.stampaVotiUguali(28)) ;
-		
 		System.out.println(this.lib.estraiVotiUguali(28)) ;
+		
+		//3_Cerca esame superato, conoscendo il nome del corso
+		String nomeCorso= "Analisi II";
+		Voto voto= lib.cercaNomeCorso(nomeCorso); //restituisce l'oggrtto voto
+		System.out.println("Il voto di "+nomeCorso+" è "+voto.getCorso());
+		
+		//4-5_ Verifica voti duplicati o in conflitto
+		
+		Voto economia2= new Voto("Economia", 24, LocalDate.now());
+		Voto economia3= new Voto("Economia", 21, LocalDate.now());
+		
+		System.out.println("Economia con 24 è duplicato: "+lib.isDuplicato(economia2)+"/ conflitto: "+lib.isConflitto(economia2));
+		System.out.println("Economia con 21 è duplicato: "+lib.isDuplicato(economia3)+"/ conflitto: "+lib.isConflitto(economia3));
+		
+		
+		//7_Migliora il libretto
+		Libretto migliorato= lib.creaLibrettoMigliorato();
+		System.out.println("Miglioramento del libretto");
+		System.out.println(lib);
+		System.out.println(migliorato);
+		
+		//8_ stampa in ordine alfabetico
+		Libretto alfabetico= new Libretto(lib);
+		alfabetico.ordinaPerCorso();
+		System.out.println(alfabetico);
+		
+		//8_ stampa in ordine di voto
+		Libretto votiDecrescenti = new Libretto(lib);
+		votiDecrescenti.ordinaPerVoto();
+		System.out.println(votiDecrescenti);
+		
+		//9_elimina voti bassi
+		lib.add(new Voto("Chimica", 19, LocalDate.now()));
+		lib.ordinaPerCorso();
+		System.out.println(lib);
+		lib.cancellaVotiScarsi();
+		System.out.println(lib);
+		
 	}
 
 	public static void main(String[] args) {
